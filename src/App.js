b8e088de7287cbabd670951813ecc02ba9de6bfd
Mark Atherton <markatherton@talkholiday.com>
2019-04-27 13:52:25 +0100

    started laying out single page app JSX added: Page-title, Reload-data and main section background

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/spacex-logo.png';
+import reload from './assets/img/refresh.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <div className="Page-title">
+          <img src={logo} className="logo" alt="space x" /> LAUNCHES
+        </div>
+
+        <div className="Reload-data">
+          Reload Data <span><img src={reload} alt="" /></span>
+        </div>
+        
       </header>
+
+      <main className="App-main">
+        
+      </main>
+
+      <footer className="App-footer">
+        
+      </footer>
+
     </div>
   );
 }
