2ab89e6694c74e76454b51cf4a0688cefca2c90e
Mark Atherton <markatherton@talkholiday.com>
2019-04-28 13:41:26 +0100

    removed unused code

--- src/App.js
+++ src/App.js
@@ -3,45 +3,6 @@ import logo from './assets/spacex-logo.png';
 import reload from './assets/img/refresh.png';
 import './App.css';
 
-const a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-fetch('https://api.spacexdata.com/v3/launches')
-  .then(response => {
-    return response.json();
-  })
-  .then(myJson => {
-    const data = JSON.stringify(myJson);
-    console.log(data);
-    return data;
-  })
-  .then(data => {
-    var JSONdata = {data};
-    console.log(JSONdata);
-  });
-
-function getData(url = `https://api.spacexdata.com/v3/launches`, data = {}) {
-  // Default options are marked with *
-    return fetch(`https://api.spacexdata.com/v3/launches`, {
-        method: "GET", // *GET, POST, PUT, DELETE, etc.
-        mode: "cors", // no-cors, cors, *same-origin
-        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: "same-origin", // include, *same-origin, omit
-        headers: {
-            "Content-Type": "application/json",
-            // "Content-Type": "application/x-www-form-urlencoded",
-        },
-        redirect: "follow", // manual, *follow, error
-        referrer: "no-referrer", // no-referrer, *client
-        //body: JSON.stringify(data),  body data type must match "Content-Type" header
-    })
-    .then(response => response.json()); // parses JSON response into native Javascript objects 
-}
-// ITS WORKING, ITS WORKING!!!
-console.log("wonder if this will get owt?", [getData()]);
-
-//const requestURL = `curl -s https://api.spacexdata.com/v3/launches/latest | jq`;
-//console.log(data);
-
 class App extends Component {
 
   constructor(props) {
