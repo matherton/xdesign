dc6b7fc6d4589bef2797ea8d135a3b6cc2769b65
Mark Atherton <markatherton@talkholiday.com>
2019-04-28 09:10:53 +0100

    changed to a class based component

--- src/App.js
+++ src/App.js
@@ -1,67 +1,97 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './assets/spacex-logo.png';
 import reload from './assets/img/refresh.png';
 import './App.css';
 
 const a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const getSpaceXData = fetch('https://api.spacexdata.com/v3/launches')
-  .then(function(response) {
+let connection;
+
+fetch('https://api.spacexdata.com/v3/launches')
+  .then(response => {
     return response.json();
   })
-  .then(function(myJson) {
-    console.log(JSON.stringify(myJson));
+  .then(myJson => {
+    const data = JSON.stringify(myJson);
+    console.log(data);
+    return data;
+  })
+  .then(data => {
+    var JSONdata = {data};
+    console.log(JSONdata);
   });
 
-const requestURL = `curl -s https://api.spacexdata.com/v3/launches/latest | jq`;
-console.log(getSpaceXData);
+function getData(url = `https://api.spacexdata.com/v3/launches`, data = {}) {
+  // Default options are marked with *
+    return fetch(`https://api.spacexdata.com/v3/launches`, {
+        method: "GET", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, cors, *same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+            "Content-Type": "application/json",
+            // "Content-Type": "application/x-www-form-urlencoded",
+        },
+        redirect: "follow", // manual, *follow, error
+        referrer: "no-referrer", // no-referrer, *client
+        //body: JSON.stringify(data),  body data type must match "Content-Type" header
+    })
+    .then(response => response.json()); // parses JSON response into native Javascript objects 
+}
+// ITS WORKING, ITS WORKING!!!
+console.log("wonder if this will get owt?", [getData()]);
 
-function App() {
-  return (
-    <div className="App">
+//const requestURL = `curl -s https://api.spacexdata.com/v3/launches/latest | jq`;
+//console.log(data);
 
-      <header className="App-header">
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
 
-        <div className="Page-title">
-          <img src={logo} className="logo" alt="space x" /> LAUNCHES
-        </div>
+        <header className="App-header">
 
-        <div className="Reload-data">
-          Reload Data <span><img src={reload} alt="" /></span>
-        </div>
-        
-      </header>
-
-      <main className="App-main">
-
-        <div className="Data-holder">
-
-          <div className="Filter-btns">
-            <button className="filter">Filter by Year</button>
-            <button className="sort">Sort Descending</button>
+          <div className="Page-title">
+            <img src={logo} className="logo" alt="space x" /> LAUNCHES
           </div>
 
-          <ul className="launches">
-            {a.map(i => {
-              return <li>
-                      <span className="Result-no">#{i}</span>
-                      <span className="Result-no">mission_name</span>
-                      <span className="Result-no">mission_date</span>
-                      <span className="Result-no">rocket_name</span>
-                    </li>
-            })}
-          </ul>
+          <div className="Reload-data">
+            Reload Data <span><img src={reload} alt="" /></span>
+          </div>
+          
+        </header>
 
-        </div>
+        <main className="App-main">
 
-      </main>
+          <div className="Data-holder">
 
-      <footer className="App-footer">
-        
-      </footer>
+            <div className="Filter-btns">
+              <button className="filter">Filter by Year</button>
+              <button className="sort">Sort Descending</button>
+            </div>
 
-    </div>
-  );
+            <ul className="launches">
+              {a.map(i => {
+                return <li>
+                        <span className="Result-no">#{i}</span>
+                        <span className="Result-no">mission_name</span>
+                        <span className="Result-no">mission_date</span>
+                        <span className="Result-no">rocket_name</span>
+                      </li>
+              })}
+            </ul>
+
+          </div>
+
+        </main>
+
+        <footer className="App-footer">
+          
+        </footer>
+
+      </div>
+    );
+  }
 }
 
 export default App;
